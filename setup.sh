ff252e0e141e32232b8e8635cca791bbee541d3d
jusan00b <babusravansiddartha@lpu.in>
2023-12-28 14:27:44 +0530

    minor changes in the script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
+sudo pacman -Syu
+clear
+
 #Basic installations using pacman
 sudo pacman -S xorg xorg-xinit
+clear
 sudo pacman -S neovim libreoffice thunar gparted
-sudo pacman -S nitrogen picom light
+clear
+sudo pacman -S nitrogen picom light pulseaudio pavucontrol
+clear
 
 #Yay installation and setup
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd
+clear
 
 #Basic installations using yay
 yay -S vscodium-bin brave-bin google-chrome-stable
+clear
 
 #MTPFS setup
 yay -S mtpfs
@@ -19,8 +27,10 @@ git clone https://aur.archlinux.org/jmtpfs.git
 cd jmtpfs
 makepkg -s
 sudo pacman -U jmtpfs*.tar.xz
+clear
 sudo pacman -Sy gvfs-mtp gvfs-gphoto2
 cd
+clear
 
 #basefiles setup
 git clone git@github.com:jusan00b/Arch-basefiles.git setup/basefiles
@@ -28,33 +38,39 @@ cp setup/basefiles/.xinitrc .xinitrc
 cp setup/basefiles/.xinitrc .bash_profile
 cp setup/basefiles/.xinitrc .bashrc
 cp setup/basefiles/anonymouspro /usr/share/fonts/anonymouspro
+clear
 
 #dwm installation and setup
 git clone git@github.com:jusan00b/Arch-DWM.git setup/dwm
 cd setup/dwm
 sudo make clean install
 cd
+clear
 
 #dmenu installation and setup
 git clone git@github.com:jusan00b/Arch-dmenu.git setup/dmenu
 cd setup/dmenu
 sudo make clean install
 cd
+clear
 
 #st installation and setup
 git clone git@github.com:jusan00b/Arch-st.git setup/st
 cd setup/st
 sudo make clean install
 cd
+clear
 
 #slstatus installation and setup
 git clone git@github.com:jusan00b/Arch-slstatus.git setup/slstatus
 cd setup/slstatus
 sudo make clean install
 cd
+clear
 
 #neovim-setup installation
 git clone git@github.com:jusan00b/neovim-setup.git .config/nvim
+clear
 
 reboot
 
