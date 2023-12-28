ccf29e6c12a0807d9b4cd6ee9a54e031a50701fe
jusan00b <babusravansiddartha@lpu.in>
2023-12-28 16:14:48 +0530

    added user input during jmtpfs installation

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
 sudo pacman -Syu
 clear
 
-#Basic installations using pacman
-sudo pacman -S xorg xorg-xinit
-clear
-sudo pacman -S neovim libreoffice thunar gparted
-clear
-sudo pacman -S nitrogen picom light pulseaudio pavucontrol
-clear
-
 #Yay installation and setup
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -17,23 +9,38 @@ cd
 clear
 
 #Basic installations using yay
-yay -S vscodium-bin brave-bin google-chrome-stable
+yay -S vscodium-bin brave-bin google-chrome
+clear
+
+
+#Basic installations using pacman
+sudo pacman -S xorg xorg-xinit
+clear
+sudo pacman -S neovim libreoffice thunar gparted
+clear
+sudo pacman -S nitrogen picom light pulseaudio pavucontrol
 clear
 
 #MTPFS setup
 yay -S mtpfs
+mkdir Downloads
 cd Downloads
 git clone https://aur.archlinux.org/jmtpfs.git
 cd jmtpfs
 makepkg -s
-sudo pacman -U jmtpfs*.tar.xz
+
+ls -al
+echo "Enter the complete name of the pkg.tar.zst file from the above list: "
+read file_name
+sudo pacman -U "$file_name"
 clear
+
 sudo pacman -Sy gvfs-mtp gvfs-gphoto2
 cd
 clear
 
 #basefiles setup
-git clone git@github.com:jusan00b/Arch-basefiles.git setup/basefiles
+git clone https://github.com/jusan00b/Arch-basefiles.git setup/basefiles
 cp setup/basefiles/.xinitrc .xinitrc
 cp setup/basefiles/.xinitrc .bash_profile
 cp setup/basefiles/.xinitrc .bashrc
@@ -41,35 +48,35 @@ cp setup/basefiles/anonymouspro /usr/share/fonts/anonymouspro
 clear
 
 #dwm installation and setup
-git clone git@github.com:jusan00b/Arch-DWM.git setup/dwm
+git clone https://github.com/jusan00b/Arch-DWM.git setup/dwm
 cd setup/dwm
 sudo make clean install
 cd
 clear
 
 #dmenu installation and setup
-git clone git@github.com:jusan00b/Arch-dmenu.git setup/dmenu
+git clone https://github.com/jusan00b/Arch-dmenu.git setup/dmenu
 cd setup/dmenu
 sudo make clean install
 cd
 clear
 
 #st installation and setup
-git clone git@github.com:jusan00b/Arch-st.git setup/st
+git clone https://github.com/jusan00b/Arch-st.git setup/st
 cd setup/st
 sudo make clean install
 cd
 clear
 
 #slstatus installation and setup
-git clone git@github.com:jusan00b/Arch-slstatus.git setup/slstatus
+git clone https://github.com/jusan00b/Arch-slstatus.git setup/slstatus
 cd setup/slstatus
 sudo make clean install
 cd
 clear
 
 #neovim-setup installation
-git clone git@github.com:jusan00b/neovim-setup.git .config/nvim
+git clone https://github.com/jusan00b/neovim-setup.git .config/nvim
 clear
 
 reboot
