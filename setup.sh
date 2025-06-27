d0ac506d10392e2a2b3f56e7a94956f49113a8f9
Siddartha Mudili <113828149+SiddarthaMudili@users.noreply.github.com>
2025-06-27 09:57:05 +0530

    Commented basefiles installation

--- setup.sh
+++ setup.sh
@@ -25,16 +25,16 @@ sudo pacman -S pulseaudio pavucontrol pamixer
 clear
 
 #basefiles setup
-git clone https://github.com/jusan00b/arch-files.git setup/files
-cp setup/files/.xinitrc .xinitrc
-cp setup/files/.bash_profile .bash_profile
-cp setup/files/.bashrc .bashrc
-cp setup/files/.zshrc .zshrc
-cp setup/files/.zprofile .zprofile
-cp setup/files/.p10k.zsh .p10k.zsh
-sudo cp -r setup/files/anonymouspro /usr/share/fonts/
-cd
-clear
+#git clone https://github.com/jusan00b/arch-files.git setup/files
+#cp setup/files/.xinitrc .xinitrc
+#cp setup/files/.bash_profile .bash_profile
+#cp setup/files/.bashrc .bashrc
+#cp setup/files/.zshrc .zshrc
+#cp setup/files/.zprofile .zprofile
+#cp setup/files/.p10k.zsh .p10k.zsh
+#sudo cp -r setup/files/anonymouspro /usr/share/fonts/
+#cd
+#clear
 
 #dwm installation and setup
 git clone https://github.com/jusan00b/dwm-setup.git setup/dwm
